6872de9c660d6302ec55f9e9d1e7b4e3337bca53
jonatas venancio <jvenancio167@hotmail.com>
2024-07-15 18:03:21 -0300

    add name and logout to user

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,18 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navbar } from "../../components/navbar"
 import { ModalInfo } from "./modalShow";
 import ModalDelete from "./modalDelete";
 import { ModalEdit } from "./modalEdit";
+import api from "../../services/api";
+import { useAuth } from "../../hooks/useAuth";
 
 const Profile = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [openModalInfo, setOpenModalInfo] = useState(false);
   const [openModalDelete, setOpenModalDelete] = useState(false);
   const [openModalEdit, setOpenModalEdit] = useState(false);
+  const [userName, setUserName] = useState('');
 
+  const { signOut } = useAuth()
+  
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
   };
+
+  useEffect(() => {
+    const getInfo = async () => {
+      try {
+        const { data } = await api.get('/client/show')
+        setUserName(data.name)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getInfo()
+  }, [])
 
   return (
     <>
@@ -23,13 +41,13 @@ const Profile = () => {
             onClick={toggleDropdown} 
             className="flex items-center justify-between w-full py-2 px-3 md:hover:bg-transparent md:border-0  md:p-0 md:w-auto md:dark:hover:text-yellow-700 dark:focus:text-yellow-600 dark:hover:bg-yellow-700 md:dark:hover:bg-transparent"
           >
-            Dropdown 
+            {userName} 
             <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
             </svg>
           </button>
           {isDropdownOpen && (
-            <div className="absolute -left-10 mt-2 z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-black dark:divide-gray-600">
+            <div className="absolute right-0 mt-2 z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-black dark:divide-gray-600">
               <ul className="py-2 text-sm dark:text-white" aria-labelledby="dropdownNavbarLink">
                 <li>
                   <button 
@@ -64,6 +82,7 @@ const Profile = () => {
                 <li>
                   <button
                     className="block px-4 py-2  dark:hover:text-yellow-600"
+                    onClick={signOut}
                   >
                     Logout
                   </button>
